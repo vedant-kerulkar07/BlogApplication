d7fec290dd9a83df1441298de552435854cf5454
22vedant22 <kerulkarvedant@gmail.com>
2026-09-18 22:28:47 +0530

    Blog by category page ui updated

--- client/src/pages/Blog/BlogByCategory.jsx
+++ client/src/pages/Blog/BlogByCategory.jsx
@@ -5,31 +5,32 @@ import { useFetch } from '@/hooks/useFetch'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { TbCategoryPlus } from "react-icons/tb";
-const BlogByCategory = () => {
-    const {category} = useParams()
-  const { data: blogData, loading, error } = useFetch(`${getEnv('VITE_API_BASE_URL')}/blog/get-blog-by-category/${category}`, {
-    method: 'get',
-    Credential: 'include'
-  },[category])
-  if (loading) return <Loading />
-  return (
-    <>
-    <div className='flex items-center gap-3 text-2xl font-bold text-violet-500 border-b pb-3 mb-5'>
-      <TbCategoryPlus/>
-    <h4>{blogData && blogData.categoryData?.name}</h4>
-    </div>
-       <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
-      {blogData && blogData.blog.length > 0
 
-        ?
-        blogData.blog.map(blog => <BlogCard key={blog._id} props={blog} />)
-        :
-        <div>Data not Found</div>
-        
-      }
-    </div>
-    </>
- 
-  )
+const BlogByCategory = () => {
+    const { category } = useParams()
+    const { data: blogData, loading, error } = useFetch(`${getEnv('VITE_API_BASE_URL')}/blog/get-blog-by-category/${category}`, {
+        method: 'get',
+        Credential: 'include'
+    }, [category])
+
+    if (loading) return <Loading />
+
+    return (
+        <>
+            <div className='flex items-center gap-3 text-xl sm:text-2xl font-bold text-[#4A3728] border-b border-[#EADFD3] pb-3 mb-5'>
+                <TbCategoryPlus className='text-[#D97748]' />
+                <h4>{blogData && blogData.categoryData?.name}</h4>
+            </div>
+            <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5 sm:gap-8 md:gap-10'>
+                {blogData && blogData.blog.length > 0
+                    ?
+                    blogData.blog.map(blog => <BlogCard key={blog._id} props={blog} />)
+                    :
+                    <div className='text-[#8C7B6B]'>Data not Found</div>
+                }
+            </div>
+        </>
+    )
 }
+
 export default BlogByCategory
