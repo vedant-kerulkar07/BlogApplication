a39dbd898ffc623a07c68d810298e5cb2a5d543b
22vedant22 <kerulkarvedant@gmail.com>
2026-09-24 22:34:09 +0530

    Comment.jsx page logic and UI updated

--- client/src/pages/Comments.jsx
+++ client/src/pages/Comments.jsx
@@ -15,7 +15,6 @@ import {
 import { useFetch } from '@/hooks/useFetch'
 import { getEnv } from '@/helpers/getEnv'
 import Loading from '@/components/Loading'
-// import { FaEdit } from "react-icons/fa";
 import { MdEdit } from "react-icons/md";
 import { RiDeleteBinFill } from "react-icons/ri";
 import { showToast } from '@/helpers/showToast'
@@ -24,20 +23,20 @@ import { comment } from 'postcss'
 
 const Comments = () => {
 
-  const [refreshData,setRefreshData]= useState(false)
+  const [refreshData, setRefreshData] = useState(false)
 
   const { data, loading, error } = useFetch(`${getEnv('VITE_API_BASE_URL')}/comment/get-all-comment`, {
     method: 'get',
     credentials: 'include'
-  },[refreshData])
+  }, [refreshData])
 
-  const handleDelete = async (id)=>{
+  const handleDelete = async (id) => {
     const response = await deleteData(`${getEnv('VITE_API_BASE_URL')}/comment/delete/${id}`)
-    if(response){
+    if (response) {
       setRefreshData(!refreshData)
-      showToast('success','Data deleted')
-    }else{
-      showToast('error','Data not deleted')
+      showToast('success', 'Data deleted')
+    } else {
+      showToast('error', 'Data not deleted')
     }
   }
 
@@ -45,45 +44,46 @@ const Comments = () => {
 
   return (
     <div>
-      <Card>
+      <Card className="bg-white border-[#EADFD3] shadow-sm">
         <CardContent>
-          <Table>
-            <TableCaption>A list of your recent invoices.</TableCaption>
-            <TableHeader>
-              <TableRow>
-                <TableHead >Blog</TableHead>
-                <TableHead>Comment By</TableHead>
-                <TableHead>Comment</TableHead>
-                <TableHead>Action</TableHead>
+          <div className="overflow-x-auto">
+            <Table>
+              <TableCaption className="text-[#8C7B6B]">A list of your recent invoices.</TableCaption>
+              <TableHeader>
+                <TableRow className="border-[#EADFD3] hover:bg-[#FFF9F2]">
+                  <TableHead className="text-[#4A3728]">Blog</TableHead>
+                  <TableHead className="text-[#4A3728]">Comment By</TableHead>
+                  <TableHead className="text-[#4A3728]">Comment</TableHead>
+                  <TableHead className="text-[#4A3728]">Action</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {data && data.comments.length > 0 ?
 
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {data && data.comments.length > 0 ?
+                  data.comments.map(comment =>
+                    <TableRow key={comment._id} className="border-[#EADFD3] hover:bg-[#FFF9F2]">
+                      <TableCell className="whitespace-nowrap text-[#4A3728]">{comment?.blogid?.title}</TableCell>
+                      <TableCell className="whitespace-nowrap text-[#4A3728]">{comment?.user?.name || 'Deleted user'}</TableCell>
+                      <TableCell className="text-[#4A3728]">{comment?.comment}</TableCell>
+                      <TableCell className="flex gap-3">
+                        <Button onClick={() => handleDelete(comment._id)} variant="outline" className="border-[#EADFD3] text-[#4A3728] hover:bg-[#D97748] hover:text-white hover:border-[#D97748]">
+                          <RiDeleteBinFill />
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  )
+                  :
 
-                data.comments.map(comment =>
-                  <TableRow key={comment._id}>
-                    <TableCell >{comment?.blogid?.title}</TableCell>
-                    <TableCell >{comment?.user?.name}</TableCell>
-                    <TableCell >{comment?.comment}</TableCell>
-                    <TableCell className="flex gap-3">
-                      <Button onClick={()=>handleDelete(comment._id)} variant="outline" className="hover:bg-violet-500 hover:text-white">
-                        <RiDeleteBinFill />
-                      </Button>
+                  <TableRow className="border-[#EADFD3]">
+                    <TableCell colSpan="4" className="text-[#8C7B6B]">
+                      Data not Found
                     </TableCell>
                   </TableRow>
-                )
-                :
 
-                <TableRow>
-                  <TableCell colSpan="3">
-                    Data not Found
-                  </TableCell>
-                </TableRow>
-
-              }
-            </TableBody>
-          </Table>
+                }
+              </TableBody>
+            </Table>
+          </div>
         </CardContent>
 
       </Card>
