e0a6c2a24f98b44f8d16e2760ff6470096ff20c6
22vedant22 <kerulkarvedant@gmail.com>
2026-09-24 23:15:13 +0530

    Index.jsx file updated...

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -12,14 +12,14 @@ const Index = () => {
   })
   if (loading) return <Loading />
   return (
-    <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10'>
+    <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5 sm:gap-8 md:gap-10'>
       {blogData && blogData.blog.length > 0
 
         ?
         blogData.blog.map(blog => <BlogCard key={blog._id} props={blog} />)
         :
-        <div>Data not Found</div>
-        
+        <div className='text-[#8C7B6B]'>Data not Found</div>
+
       }
     </div>
   )
